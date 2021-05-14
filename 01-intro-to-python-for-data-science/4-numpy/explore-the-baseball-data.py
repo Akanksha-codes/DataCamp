7007ab835ed4426fbdcc7533dafd606d6988a3c0
Akanksha Gautam <68818469+Akanksha-codes@users.noreply.github.com>
2021-05-15 00:11:26 +0530

    Update explore-the-baseball-data.py
    it works in similar way

--- 01-intro-to-python-for-data-science/4-numpy/explore-the-baseball-data.py
+++ 01-intro-to-python-for-data-science/4-numpy/explore-the-baseball-data.py
@@ -23,16 +23,16 @@ import numpy as np
 
 # Print mean height (first column)
 avg = np.mean(np_baseball[:,0])
-print("Average: " + str(avg))
+print(np.mean(avg))
 
 # Print median height. Replace 'None'
 med = np.median(np_baseball[:,0])
-print("Median: " + str(med))
+print(np.median(med))
 
 # Print out the standard deviation on height. Replace 'None'
 stddev = np.std(np_baseball[:,0])
-print("Standard Deviation: " + str(stddev))
+print(np.std(stddev))
 
 # Print out correlation between first and second column. Replace 'None'
 corr = np.corrcoef(np_baseball[:,0], np_baseball[:,1])
-print("Correlation: " + str(corr))
+print(np.corrcoef(corr))
